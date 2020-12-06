b8857731055bf15211ae63aa323c649511ed28c0
Whispers12 <frolovandrey752@gmail.com>
2020-12-06 21:32:55 +0400

    fix

---
+++ apps/streamer/lib/streamer/binance.ex
@@ -0,0 +1,14 @@
+defmodule Streamer.Binance do
+  use WebSockex
+
+  @stream_endpoint "wss://stream.binance.com:9443/ws/"
+
+  def start_link(symbol, state) do
+    WebSockex.start_link("#{@stream_endpoint}#{symbol}@trade", __MODULE__, state)
+  end
+
+  def handle_frame({type, msg}, state) do
+    IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
+    {:ok, state}
+  end
+end
