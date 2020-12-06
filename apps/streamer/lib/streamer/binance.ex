daf386cb1c46fb71c0581e0ab674797f3897f043
Whispers12 <frolovandrey752@gmail.com>
2020-12-06 21:54:14 +0400

    add naive trader

--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -7,8 +7,30 @@ defmodule Streamer.Binance do
     WebSockex.start_link("#{@stream_endpoint}#{symbol}@trade", __MODULE__, state)
   end
 
-  def handle_frame({type, msg}, state) do
-    IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
+  def handle_frame({_type, msg}, state) do
+    case Jason.decode(msg) do
+      {:ok, event} -> handle_event(event, state)
+      {:error, _} -> throw("Unable to parse message: #{msg}")
+    end
+
+    # IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
     {:ok, state}
+  end
+
+  def handle_event(%{"e" => "trade"} = event, _state) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    IO.inspect(trade_event, label: "Trade event received ")
   end
 end
